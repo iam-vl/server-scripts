4884b301ff544a7bc757ffc896911bc497401d58
vl <gztrk@engineer.com>
2025-07-08 14:25:53 +0300

    add 3x compose

--- docker/install_docker.sh
+++ docker/install_docker.sh
@@ -6,7 +6,7 @@ for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker c
 # Install thru apt: Set up Docker's apt repository.
 #   Add Docker's official GPG key:
 sudo apt-get update
-sudo apt-get install ca-certificates curl
+sudo apt-get install ca-certificates curl 
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
