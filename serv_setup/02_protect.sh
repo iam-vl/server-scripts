06dd8790231bf44b206cdcf858a77c4407b87be6
vl <gztrk@engineer.com>
2025-07-08 13:11:39 +0300

    add docker installation

--- serv_setup/02_protect.sh
+++ serv_setup/02_protect.sh
@@ -8,3 +8,14 @@ add user user123
 # Add the user to sudo group 
 usermod -aG sudo user123
 su - user123
+
+
+# To block root access thru ssh: 
+# sudo nano /etc/ssh/sshd_config 
+# Change val: Permit root login: no 
+
+# apply the configs  
+# sudo service sshd restart 
+
+
+
